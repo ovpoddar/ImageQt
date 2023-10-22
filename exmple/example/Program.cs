339f33f4f01a4f228f9a1c269f8227be0e799281
Ov poddar Windows <ayanpoddar0@gmail.com>
2023-10-22 16:10:37 +0530

    preparing for linux

--- exmple/example/Program.cs
+++ exmple/example/Program.cs
@@ -4,20 +4,28 @@ using System.Text;
 
 var qt = new ImageQt.ImageQt("Open window.");
 
-var bim = new Bitmap(@"side-view-smiley-woman-holding-hen.jpg");
-var fs = new MemoryStream();
-fs.Seek(0, SeekOrigin.Begin);
-for (var y = 0; y < bim.Height; y++)
-{
-    for (var x = 0; x < bim.Width; x++)
-    {
-        var color = bim.GetPixel(x, y).ToArgb();
-        fs.Write(BitConverter.GetBytes(color));
-    }
-}
-fs.Close();
-var by = fs.ToArray();
+/* generate bytes
+ * var bim = new Bitmap(@"side-view-smiley-woman-holding-hen.jpg");
+ * var fs = new MemoryStream();
+ * fs.Seek(0, SeekOrigin.Begin);
+ * for (var y = 0; y < bim.Height; y++)
+ * {
+ *      for (var x = 0; x < bim.Width; x++)
+ *      {
+ *          var color = bim.GetPixel(x, y).ToArgb();
+ *          fs.Write(BitConverter.GetBytes(color));
+ *      }
+ *  }
+ *  fs.Close();
+ *  var by = fs.ToArray(); 
+ */
 
-qt.GenerateTheBitMap(bim.Width, bim.Height, ref by);
+
+int width = 1000,
+    height = 667;
+var bytes = File.ReadAllBytes("bytes");
+
+qt.GenerateTheBitMap(width, height, ref bytes);
+
 await qt.Run(true);
 Console.ReadLine();
