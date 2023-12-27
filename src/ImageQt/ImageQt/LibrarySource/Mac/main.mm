a977d12d18dcbf90aa1fef57715719bdf78a82f5
Ov poddar Windows <ayanpoddar0@gmail.com>
2023-12-28 02:31:59 +0530

    for c++ try some

--- src/ImageQt/ImageQt/LibrarySource/Mac/main.mm
+++ src/ImageQt/ImageQt/LibrarySource/Mac/main.mm
@@ -22,3 +22,23 @@ extern "C" {
             bitsPerPixel:sizeof(Pixel) * 8];
     }
 }
+/*
+
+#include <string>
+
+class NSBitmapImageRep {
+public:
+    NSBitmapImageRep(unsigned char** bitmapDataPlanes, int pixelsWide, int pixelsHigh, int bitsPerSample, int samplesPerPixel, bool hasAlpha, bool isPlanar, std::string colorSpaceName, int bytesPerRow, int bitsPerPixel) {
+        // Constructor implementation
+    }
+};
+
+struct Pixel {
+    // Define the Pixel struct if not already defined
+};
+
+NSBitmapImageRep* CreateImageWithHeightWidth(int width, int height, unsigned char* buffer, std::string colorName) {
+    return new NSBitmapImageRep(&buffer, width, height, 8, 4, true, false, colorName, width * sizeof(Pixel), sizeof(Pixel) * 8);
+}
+
+*/
