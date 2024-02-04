dcb1e14985f81efa7f2af492a02480c0e11137d2
Ov poddar Windows <ayanpoddar0@gmail.com>
2024-02-05 00:51:06 +0530

    update the code

--- src/Handler/Linux/Window.cs
+++ src/Handler/Linux/Window.cs
@@ -9,7 +9,6 @@ internal class Window : IWindow
     private ulong _x11Window;
     private IntPtr _graphicsContext;
     private IntPtr _image;
-    private ulong _pixmap;
     private IntPtr _visual;
     private uint _depth;
 
@@ -40,7 +39,10 @@ internal class Window : IWindow
             throw new Exception("Could not Initialize.");
         XLib.XMapWindow(window, _x11Window);
     }
-
+    public void LoadBitMap(int width, int height, ref nint imageData, IntPtr display)
+    {
+        _image = XLib.XCreateImage(display, _visual, _depth, ImageFormat.ZPixmap, 0, imageData, (uint)width, (uint)height, 32, 0);
+    }
     public void ProcessEvent(IntPtr window)
     {
         var ev = Marshal.AllocHGlobal(192);
@@ -63,12 +65,17 @@ internal class Window : IWindow
         }
 
     }
+    void DrawImageFromPointer(IntPtr display)
+    {
+        if (_image == IntPtr.Zero
+            || display == IntPtr.Zero)
+            return;
+        var image = Marshal.PtrToStructure<XImage>(_image);
 
+        XLib.XPutImage(display, _x11Window, _graphicsContext, _image, 0, 0, 0, 0, (uint)image.width, (uint)image.height);
+    }
     public void CleanUpResources(ref IntPtr window)
     {
-        if (_pixmap != default)
-            XLib.XFreePixmap(window, _pixmap);
-
         if (_graphicsContext != default)
             XLib.XFreeGC(window, _graphicsContext);
 
@@ -77,21 +84,5 @@ internal class Window : IWindow
 
     }
 
-    void DrawImageFromPointer(IntPtr display)
-    {
-        if (_image == IntPtr.Zero
-            || _pixmap == default
-            || display == IntPtr.Zero)
-            return;
-        var image = Marshal.PtrToStructure<XImage>(_image);
 
-        XLib.XPutImage(display, _pixmap, _graphicsContext, _image, 0, 0, 0, 0, (uint)image.width, (uint)image.height);
-        XLib.XCopyArea(display, _pixmap, _x11Window, _graphicsContext, 0, 0, (uint)image.width, (uint)image.height, 0, 0);
-    }
-
-    public void LoadBitMap(int width, int height, ref nint imageData, IntPtr display)
-    {
-        _image = XLib.XCreateImage(display, _visual, _depth, ImageFormat.ZPixmap, 0, imageData, (uint)width, (uint)height, 32, 0);
-        _pixmap = XLib.XCreatePixmap(display, _x11Window, (uint)width, (uint)height, _depth);
-    }
 }
