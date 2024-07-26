589279530e87878b55330af1a4b5db69b8d41866
Ov poddar Windows <ayanpoddar0@gmail.com>
2024-07-26 17:06:01 +0530

    added convinience

--- Src/ImageQT/ImageQt.cs
+++ Src/ImageQT/ImageQt.cs
@@ -14,14 +14,33 @@ public class ImageQt
 
     public ImageQt(Image image)
     {
+#if DEBUG
+        if (OperatingSystem.IsWindows())
+            _windowManager = new Handlers.Window.WindowManager();
+        else if (OperatingSystem.IsLinux())
+            _windowManager = new Handlers.Linux.WindowManager();
+        else if (OperatingSystem.IsMacCatalyst())
+            _windowManager = new Handlers.Mac.WindowManager();
+        else
+            throw new PlatformNotSupportedException();
+#else
+#if Windows
         _windowManager = new Handlers.Window.WindowManager();
+#elif OSX
+            _windowManager = new Handlers.Mac.WindowManager();
+#elif Linux
+            _windowManager = new Handlers.Linux.WindowManager();
+#else
+            throw new PlatformNotSupportedException();
+#endif
+#endif
         _windowManager.CreateWindow((uint)image.Height, (uint)image.Width);
         _windowManager.SetUpImage(image);
     }
 
     public async Task Show(TimeSpan? time = null)
     {
-        if(_disposed)
+        if (_disposed)
             throw new AlreadyExecutedException();
 
         await _windowManager.Show(time.HasValue ? DateTime.Now.Add(time.Value) : null);
