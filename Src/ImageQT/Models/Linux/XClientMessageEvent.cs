09d754652aa4b3876d87086690ce9e0cc5d64e89
Ov poddar Windows <ayanpoddar0@gmail.com>
2024-07-26 18:55:12 +0530

    seperate classed

--- Src/ImageQT/Models/Linux/XClientMessageEvent.cs
+++ Src/ImageQT/Models/Linux/XClientMessageEvent.cs
@@ -15,15 +15,4 @@ public struct XClientMessageEvent
     public int format;
     public XClientMessageEventData data;
 }
-
-[StructLayout(LayoutKind.Explicit)]
-public struct XClientMessageEventData
-{
-    [FieldOffset(0)]
-    public IntPtr b; // If format == 8
-    [FieldOffset(0)]
-    public ushort s; // If format == 16
-    [FieldOffset(0)]
-    public int l;    // If format == 32
-}
 #endif
---
+++ Src/ImageQT/Models/Linux/XClientMessageEventData.cs
@@ -0,0 +1,16 @@
+﻿#if DEBUG || Linux
+using System.Runtime.InteropServices;
+
+namespace ImageQT.Models.Linux;
+
+[StructLayout(LayoutKind.Explicit)]
+public struct XClientMessageEventData
+{
+    [FieldOffset(0)]
+    public IntPtr b; // If format == 8
+    [FieldOffset(0)]
+    public ushort s; // If format == 16
+    [FieldOffset(0)]
+    public int l;    // If format == 32
+}
+#endif
