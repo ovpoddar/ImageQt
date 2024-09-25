e3587d8c88add9f0d0d8c985809e9531bc373efe
Ov poddar Windows <ayanpoddar0@gmail.com>
2024-09-26 04:35:50 +0530

    secetion for improvement

--- Src/ImageQT/Decoder/BMP/Models/ColorReader/RgbColorReader.cs
+++ Src/ImageQT/Decoder/BMP/Models/ColorReader/RgbColorReader.cs
@@ -72,7 +72,7 @@ internal class RgbColorReader : BaseColorReader
                 /* TODO:Issue: according to Wikipidia 
                  * The 32-bit per pixel (32bpp) format supports 4,294,967,296 distinct colors and stores 1 pixel per 4-byte DWORD.
                  * Each DWORD can define the alpha, red, green and blue samples of the pixel.
-                 * but found images in bgra format
+                 * but found images in bgra format also can have MemoryMarshal.Cast if the format is bgra
                  * */
                 Debug.Assert(pixel.Length == 4);
                 result[writingIndex++] = new Pixels(pixel[2], pixel[1], pixel[0]);
