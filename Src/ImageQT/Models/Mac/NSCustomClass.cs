e55283fb6ca11a9ae2cc04ca214514d9e92330bc
Ov poddar Windows <ayanpoddar0@gmail.com>
2024-11-13 01:55:22 +0530

    getting new ns custome class each time

--- Src/ImageQT/Models/Mac/NSCustomClass.cs
+++ Src/ImageQT/Models/Mac/NSCustomClass.cs
@@ -12,30 +12,29 @@ namespace ImageQT.Models.Mac;
 
 internal class NSCustomClass : SafeHandleBaseZeroInvalid
 {
-    private readonly IntPtr _customClassPointer;
     public NSCustomClass(windowWillClose actionDelegate) : base(true)
     {
-        //TODO:INVISTAGATE ObjectCRuntime.ObjCGetClass("NSImageView") method can return the same _customClassPointer so might not need to store a pointer.
         var nsObjectClass = ObjectCRuntime.ObjCGetClass("NSObject");
-        _customClassPointer = ObjectCRuntime.ObjCAllocateClassPair(nsObjectClass, "NSCustomClass", 0);
+        var nsCustomClass = ObjectCRuntime.ObjCAllocateClassPair(nsObjectClass, "NSCustomClass", 0);
         if (nsObjectClass == IntPtr.Zero)
         {
             Debug.WriteLine("fail to create class.find a unique name for this class");
             Debug.Assert(nsObjectClass != IntPtr.Zero);
         }
         var methodSelector = ObjectCRuntime.SelGetUid("windowWillClose:");
-        ObjectCRuntime.ClassAddMethod(_customClassPointer, methodSelector, actionDelegate, "V@:@");
-        ObjectCRuntime.ObjCRegisterClassPair(_customClassPointer);
+        ObjectCRuntime.ClassAddMethod(nsCustomClass, methodSelector, actionDelegate, "V@:@");
+        ObjectCRuntime.ObjCRegisterClassPair(nsCustomClass);
 
-        var appDelegateInstance = ObjectCRuntime.PointerObjCMsgSend(_customClassPointer, PreSelector.Alloc);
+        var appDelegateInstance = ObjectCRuntime.PointerObjCMsgSend(nsCustomClass, PreSelector.Alloc);
         SetHandle(ObjectCRuntime.PointerObjCMsgSend(appDelegateInstance, PreSelector.Init));
     }
 
     protected override bool ReleaseHandle()
     {
+        // do not rearrange order matter here.
         var response = base.ReleaseHandle();
-        if (_customClassPointer != IntPtr.Zero)
-            ObjectCRuntime.ObjCDisposeClassPair(_customClassPointer);
+        var nsCustomClass = ObjectCRuntime.ObjCGetClass("NSCustomClass");
+        ObjectCRuntime.ObjCDisposeClassPair(nsCustomClass);
         return response;
     }
 }
