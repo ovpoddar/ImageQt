f7b82d8fd46329e14442ec646fa2eb6f567cd6c6
Ov poddar Windows <ayanpoddar0@gmail.com>
2024-11-16 06:56:56 +0530

    seperate the image class

--- Src/ImageQT/Models/ImagqQT/Image.cs
+++ Src/ImageQT/Models/ImagqQT/Image.cs
@@ -4,11 +4,6 @@ using System.Runtime.InteropServices;
 
 namespace ImageQT.Models.ImagqQT;
 
-internal sealed record Image<T>(int Width, int Height, int Mipmaps, int Format, short BitCount, T[] ActualBytes) : Image(Width, Height, Mipmaps, Format, BitCount)
-{
-    public override IntPtr Id => Marshal.UnsafeAddrOfPinnedArrayElement(ActualBytes, 0);
-}
-
 public abstract record Image(int Width, int Height, int Mipmaps, int Format, short BitCount)
 {
     public abstract IntPtr Id { get; }
---
+++ Src/ImageQT/Models/ImagqQT/ImageOfT.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ImageQT.Models.ImagqQT;
+
+internal sealed record Image<T>(int Width, int Height, int Mipmaps, int Format, short BitCount, T[] ActualBytes) : Image(Width, Height, Mipmaps, Format, BitCount)
+{
+    public override IntPtr Id => Marshal.UnsafeAddrOfPinnedArrayElement(ActualBytes, 0);
+}
