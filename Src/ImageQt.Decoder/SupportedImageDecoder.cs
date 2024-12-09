e0342f1e22ca8dd8a1352154574e8c7de367bd2f
Ov poddar Windows <ayanpoddar0@gmail.com>
2024-12-10 03:13:02 +0530

    make sure its compilable for aot build

--- Src/ImageQt.Decoder/SupportedImageDecoder.cs
+++ Src/ImageQt.Decoder/SupportedImageDecoder.cs
@@ -6,19 +6,11 @@ internal class SupportedImageDecoder
 {
     internal static IEnumerable<IImageDecoder> GetSupportedDecoders(Stream stream)
     {
-#if AOT
         var types = new List<IImageDecoder>()
         {
             new BmpDecoder(stream),
             new PngDecoder(stream)
         };
-#else
-         var types = typeof(SupportedImageDecoder).Assembly.GetTypes()
-                    .Where(a => a.IsClass
-                        && a.GetInterfaces().Contains(typeof(IImageDecoder)))
-                    .Select(a => Activator.CreateInstance(a, stream))
-                    .Cast<IImageDecoder>();
-#endif
         return types;
     }
 
