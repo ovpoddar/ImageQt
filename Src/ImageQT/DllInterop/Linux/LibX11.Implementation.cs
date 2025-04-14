267067b41a00bdf8dd17d4fd2b358ec9647f47a1
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-04-15 02:55:32 +0530

    Enhance LibX11 class with new methods and updates
    This commit introduces several new methods to the `LibX11` class for better interaction with the X11 display server, including implementations for `XDefaultScreen`, `XRootWindow`, `XDefaultVisual`, and `XDefaultDepth`. The `depth` parameter type for `XCreateImage` and `XCreatePixmap` has been changed from `uint` to `int`. Additionally, new resource management methods such as `XFreePixmap`, `XFreeGC`, and `XFree` have been added. The `using` directive for `System.Runtime.InteropServices` has also been included to facilitate P/Invoke functionality.

--- Src/ImageQT/DllInterop/Linux/LibX11.Implementation.cs
+++ Src/ImageQT/DllInterop/Linux/LibX11.Implementation.cs
@@ -1,6 +1,7 @@
 ﻿#if DEBUG || Linux
 using ImageQT.Models.Linux;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace ImageQT.DllInterop.Linux;
 internal unsafe partial class LibX11
@@ -15,5 +16,18 @@ internal unsafe partial class LibX11
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ulong XWhitePixel(IntPtr display, int screen) =>
         ScreenOfDisplay(display, screen)->white_pixel;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int XDefaultScreen(IntPtr display) =>
+        ((XPrivateDisplay*)display.ToPointer())->default_screen;
+
+    public static ulong XRootWindow(IntPtr display, int screen) =>
+        ScreenOfDisplay(display, screen)->root;
+
+    public static IntPtr XDefaultVisual(IntPtr display, int screen) =>
+        ScreenOfDisplay(display, screen)->root_visual;
+
+    public static int XDefaultDepth(IntPtr display, int screen) =>
+        ScreenOfDisplay(display, screen)->root_depth;
 }
 #endif
--- Src/ImageQT/DllInterop/Linux/LibX11.cs
+++ Src/ImageQT/DllInterop/Linux/LibX11.cs
@@ -8,12 +8,11 @@ internal partial class LibX11
     private const string _dllName = "libX11.so";
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/OpenDis.c 500 line changes
     public static partial IntPtr XOpenDisplay([MarshalAs(UnmanagedType.LPWStr)] string? displayName);
 
     [LibraryImport(_dllName)]
-    public static partial int XDefaultScreen(IntPtr display);
-
-    [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/CrWindow.c
     public static partial ulong XCreateSimpleWindow(
        IntPtr display,
        ulong parentWindow,
@@ -27,40 +26,39 @@ internal partial class LibX11
    );
 
     [LibraryImport(_dllName)]
-    public static partial ulong XRootWindow(IntPtr display, int screen);
-
-    [LibraryImport(_dllName)]
-    public static partial IntPtr XDefaultVisual(IntPtr display, int screen);
-
-    [LibraryImport(_dllName)]
-    public static partial uint XDefaultDepth(IntPtr display, int screen);
-
-    [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/SelInput.c
     public static partial int XSelectInput(IntPtr display, ulong window, EventMask eventMask);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/IntAtom.c
     public static partial ulong XInternAtom(IntPtr display, [MarshalAs(UnmanagedType.LPStr)] string name, [MarshalAs(UnmanagedType.Bool)] bool state);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/MapWindow.c
     public static partial int XMapWindow(IntPtr display, ulong window);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/NextEvent.c
     public static partial int XNextEvent(IntPtr display, IntPtr xEvent);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/SetWMProto.c
     public static partial int XSetWMProtocols(IntPtr display, ulong window, IntPtr atom, int count);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/CrGC.c
     public static partial IntPtr XCreateGC(IntPtr display, ulong window, ulong valueMask, IntPtr values);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/ClDisplay.c
     public static partial int XCloseDisplay(IntPtr display);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/ImUtil.c
     public static partial IntPtr XCreateImage(
        IntPtr display,
        IntPtr visual,
-       uint depth,
+       int depth,
        ImageFormat format,
        int offset,
        IntPtr data,
@@ -75,7 +73,7 @@ internal partial class LibX11
        ulong window,
        uint width,
        uint height,
-       uint depth);
+       int depth);
 
     [LibraryImport(_dllName)]
     public static partial void XPutImage(IntPtr display,
@@ -102,12 +100,15 @@ internal partial class LibX11
       int destY);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/FreePix.c
     public static partial void XFreePixmap(IntPtr display, ulong pixmap);
 
     [LibraryImport(_dllName)]
+    // /home/ayan/projects/libx11/src/FreeGC.c
     public static partial void XFreeGC(IntPtr display, IntPtr gc);
 
     [LibraryImport(_dllName)]
+    // this is a wrapper for stdlib.h free call
     public static partial void XFree(IntPtr display);
 }
 #endif
